acd2d6b666349bada41cd6570f19e0d9310bf445
juancho <yesid.eng15@gmail.com>
2024-04-30 21:30:31 -0500

    300420246

--- src/functions/Time/timer.js
+++ src/functions/Time/timer.js
@@ -1,10 +1,10 @@
 import moment from "moment-timezone"
 const getDateNow=()=>{ 
-    return moment().format()//.tz('America/Lima').utc(-5).format();
+    return moment().subtract(5, 'hours').format()//.tz('America/Lima').utc(-5).format();
 }
 
 const getDate=(strText,Format)=>{ 
-    return moment().format()//moment(strText,Format).format()//.tz('America/Lima').utc(-5).format();
+    return moment().subtract(5, 'hours').format()//moment(strText,Format).format()//.tz('America/Lima').utc(-5).format();
 }
 
 const getDiffTimeSeconds=(strText)=>{ 
